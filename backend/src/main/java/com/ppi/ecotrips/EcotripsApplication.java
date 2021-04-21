49f209a058aa0ae44c8522200ecb5106c2d5e7e7
Juan Jose Garcia <juangarciavxx@gmail.com>
2021-04-20 23:54:36 -0500

    subiendo archivos iniciales de vue y spring para front y back

---
+++ backend/src/main/java/com/ppi/ecotrips/EcotripsApplication.java
@@ -0,0 +1,13 @@
+package com.ppi.ecotrips;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcotripsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EcotripsApplication.class, args);
+    }
+
+}
---
+++ backend/src/test/java/com/ppi/ecotrips/EcotripsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ppi.ecotrips;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcotripsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
