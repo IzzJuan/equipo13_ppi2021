140ea67e15c57222286b287d86121a93f003288f
Juan Jose Garcia <juangarciavxx@gmail.com>
2021-04-22 13:48:31 -0500

    primera version de la navbar

--- frontend/src/components/NavBar.vue
+++ frontend/src/components/NavBar.vue
@@ -1,10 +1,30 @@
 <template>
   <div>
-    <nav class="navbar navbar-light bg-light">
-      <a class="navbar-brand" href="#">
-        <img src="/docs/4.0/assets/brand/bootstrap-solid.svg" width="30" height="30" class="d-inline-block align-top" alt="">
-        Bootstrap
-      </a>
+    <nav class="navbar navbar-dark bg-primary navbar-expand-lg">
+      <router-link to="/" class="navbar-brand mb-0 h1">
+        <img src="../assets/img/EcoTripsPNG.png" class="img-fluid" width="100" height="auto" alt="Ecotrips logo">
+      </router-link>
+      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarTogglerDemo02"
+              aria-controls="navbarTogglerDemo02" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarTogglerDemo02">
+        <ul class="navbar-nav mr-auto mt-2 mt-lg-0">
+          <li class="nav-item active d-flex">
+            <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+          </li>
+          <li class="nav-item d-flex">
+            <a class="nav-link" href="#">Link</a>
+          </li>
+          <li class="nav-item d-flex">
+            <a class="nav-link disabled" href="#">Disabled</a>
+          </li>
+        </ul>
+        <form class="form-inline my-2 my-lg-0">
+          <input class="form-control mr-sm-2" type="search" placeholder="Search">
+          <button class="btn btn-outline-light my-2 my-sm-0" type="submit">Search</button>
+        </form>
+      </div>
     </nav>
   </div>
 </template>
@@ -12,9 +32,9 @@
 <script>
 export default {
   name: "NavBar"
-}
+};
 </script>
 
-<style scoped>
+<style>
 
 </style>
