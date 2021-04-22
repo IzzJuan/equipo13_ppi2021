042a90fcde79f4b0699a52a0739fce0c7627af32
Juan Jose Garcia <juangarciavxx@gmail.com>
2021-04-22 16:16:41 -0500

    finalizando navbar conn funcionalidad de rutas y agregando el .ico a la web

--- frontend/src/components/NavBar.vue
+++ frontend/src/components/NavBar.vue
@@ -1,8 +1,11 @@
 <template>
   <div>
-    <nav class="navbar navbar-dark bg-primary navbar-expand-lg">
+    <nav class="navbar navbar-dark navbar-expand-lg">
       <router-link to="/" class="navbar-brand mb-0 h1">
-        <img src="../assets/img/EcoTripsPNG.png" class="img-fluid" width="100" height="auto" alt="Ecotrips logo">
+        <img src="../assets/img/EcoTrips_logo/BackgroundEcoTrips_logo_V2.png" class="img-fluid" width="45px"
+             height="auto" alt="Ecotrips logo">
+        <img src="../assets/img/EcoTrips_logo/TextoLogo2.png" class="img-fluid mb-2" width="100px" height="auto"
+             alt="Ecotrips logo">
       </router-link>
       <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarTogglerDemo02"
               aria-controls="navbarTogglerDemo02" aria-expanded="false" aria-label="Toggle navigation">
@@ -10,14 +13,19 @@
       </button>
       <div class="collapse navbar-collapse" id="navbarTogglerDemo02">
         <ul class="navbar-nav mr-auto mt-2 mt-lg-0">
-          <li class="nav-item active d-flex">
-            <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+          <li class="nav-item d-flex">
+            <router-link to="/about">
+            <a class="nav-link" href="/about">Inicio</a>
+            </router-link>
           </li>
           <li class="nav-item d-flex">
-            <a class="nav-link" href="#">Link</a>
+            <a class="nav-link" href="#">Reseñas</a>
           </li>
           <li class="nav-item d-flex">
-            <a class="nav-link disabled" href="#">Disabled</a>
+            <a class="nav-link" href="#">Lugares</a>
+          </li>
+          <li class="nav-item d-flex">
+            <a class="nav-link" href="#">Reservar</a>
           </li>
         </ul>
         <form class="form-inline my-2 my-lg-0">
@@ -36,5 +44,11 @@ export default {
 </script>
 
 <style>
+div nav {
+  background-color: rgb(0, 190, 201);
+}
 
+form input, button {
+  font-size: 1.5rem !important;
+}
 </style>
