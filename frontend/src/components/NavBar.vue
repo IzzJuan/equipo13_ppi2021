1fee26bcf2145452ac6998a92d897d8871cdaadd
Juan Jose Garcia <juangarciavxx@gmail.com>
2021-04-21 21:45:28 -0500

    creando navabar y implementando vue router

---
+++ frontend/src/components/NavBar.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <nav class="navbar navbar-light bg-light">
+      <a class="navbar-brand" href="#">
+        <img src="/docs/4.0/assets/brand/bootstrap-solid.svg" width="30" height="30" class="d-inline-block align-top" alt="">
+        Bootstrap
+      </a>
+    </nav>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "NavBar"
+}
+</script>
+
+<style scoped>
+
+</style>
