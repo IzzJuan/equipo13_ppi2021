a9b3ea5dac69d2530bb197e71290690b6f3e3d3f
Juan Jose Garcia <juangarciavxx@gmail.com>
2021-05-19 00:26:38 -0500

    terminando de maquetar el registro de usuario

--- frontend/src/components/SigninForm.vue
+++ frontend/src/components/SigninForm.vue
@@ -1,27 +1,30 @@
 <template lang="">
-  <div>
+  <div class="row">
     <form>
-      <div class="form-inner">
+      <div class="form-inner col">
         <h2>Signup</h2>
         <div class="form-group">
-          <label htmlFor="name">Name:</label>
-          <input type="text" name="name" id="name" />
+          <label htmlFor="name">Nombre:</label>
+          <input type="text" name="first-name" id="first-name" />
         </div>
-        <div className="form-group">
+        <div class="form-group">
+          <label htmlFor="name">Apellido:</label>
+          <input type="text" name="last-name" id="last-name" />
+        </div>
+        <div class="form-group">
+          <label htmlFor="name">Documento de Identidad:</label>
+          <input type="text" name="idUser" id="idUser" />
+        </div>
+        <div class="form-group">
           <label htmlFor="email">Email:</label>
           <input type="email" name="email" id="email" />
         </div>
-        <div className="form-group">
+        <div class="form-group">
           <label htmlFor="password">Password:</label>
           <input type="password" name="password" id="password" />
         </div>
         <input type="submit" value="Signup" />
-        <input
-          type="button"
-          onClick="{()"
-          value="Go to
-        Login"
-        />
+        <input type="button" value="Go to Login" />
       </div>
     </form>
   </div>
@@ -29,4 +32,6 @@
 <script>
 export default {};
 </script>
-<style scoped></style>
+<style scoped>
+@import "./Styles/AuthForms.css";
+</style>
